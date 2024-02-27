1fc2e2cb6e13d2fb9c6ba8949a350d99e05fdeec
Flávio Mendonça <flaviomhenrique@gmail.com>
2024-02-27 18:00:58 -0300

    delete comment done

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,7 +4,11 @@ import avatar from "../assets/woman.jpg";
 import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "./Avatar";
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <>
       <div className={styles.comment}>
@@ -23,7 +27,7 @@ export function Comment({ content }) {
                 </time>
               </div>
 
-              <button title="Deletar comentário">
+              <button onClick={handleDeleteComment} title="Deletar comentário">
                 <Trash size={24} />
               </button>
             </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -35,6 +35,13 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter((comment) => {
+      return comment !== commentToDelete;
+    });
+    setComments(commentsWithoutDeletedOne);
+  }
+
   return (
     <>
       <article className={styles.post}>
@@ -56,15 +63,14 @@ export function Post({ author, publishedAt, content }) {
           </time>
         </header>
 
+        {/* Post content */}
         <div className={styles.content}>
           {content.map((line) => {
             if (line.type === "paragraph") {
-              // eslint-disable-next-line react/jsx-key
-              return <p>{line.content}</p>;
+              return <p key={line.content}>{line.content}</p>;
             } else if (line.type === "link") {
               return (
-                // eslint-disable-next-line react/jsx-key
-                <p>
+                <p key={line.content}>
                   <a href="#">{line.content}</a>
                 </p>
               );
@@ -72,6 +78,7 @@ export function Post({ author, publishedAt, content }) {
           })}
         </div>
 
+        {/* Comment section */}
         <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
           <strong> Deixe seu comentário </strong>
           <textarea
@@ -88,8 +95,13 @@ export function Post({ author, publishedAt, content }) {
 
         <div className={styles.commentList}>
           {comments.map((comment) => {
-            // eslint-disable-next-line react/jsx-key
-            return <Comment content={comment} />;
+            return (
+              <Comment
+                key={comment}
+                content={comment}
+                onDeleteComment={deleteComment}
+              />
+            );
           })}
         </div>
       </article>
