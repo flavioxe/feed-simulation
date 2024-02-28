0d2730d2092ccca07d877eaffbb81ce5c1bc4e5c
Flávio Mendonça <flaviomhenrique@gmail.com>
2024-02-28 08:25:19 -0300

    sticky sidebar done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,30 +1,30 @@
-import { Avatar } from './Avatar'
-import { PencilLine } from 'phosphor-react'
+import { Avatar } from "./Avatar";
+import { PencilLine } from "phosphor-react";
 
-import styles from './sidebar.module.css'
-import cover from '../assets/cover.jpg'
-import avatar from '../assets/avatar.jpg'
+import styles from "./sidebar.module.css";
+import cover from "../assets/cover.jpg";
+import avatar from "../assets/avatar.jpg";
 
 export function Sidebar() {
-    return (
-        <>
-            <aside className={styles.sidebar}>
-                <img src={cover} className={styles.cover} alt="Cover image" />
+  return (
+    <>
+      <aside id="sidebar" className={styles.sidebar}>
+        <img src={cover} className={styles.cover} alt="Cover image" />
 
-                <div className={styles.profile}>
-                    <Avatar src={avatar} />
-                  
-                    <strong>Flávio Mendonça</strong>
-                    <span>Web Developer</span>
-                </div>
+        <div className={styles.profile}>
+          <Avatar src={avatar} />
 
-                <footer>
-                    <a href='#'> 
-                        <PencilLine size={20} />
-                        Editar seu perfil
-                    </a>
-                </footer>
-            </aside>
-        </>
-    )
+          <strong>Flávio Mendonça</strong>
+          <span>Web Developer</span>
+        </div>
+
+        <footer>
+          <a href="#">
+            <PencilLine size={20} />
+            Editar seu perfil
+          </a>
+        </footer>
+      </aside>
+    </>
+  );
 }
